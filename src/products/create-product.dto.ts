f13d667a1c0d1c5b1958a4d8e5990a8d80ff5792
Donpichalinda <onlygameswhitme@gmail.com>
2025-10-21 17:51:15 -0500

    Add CreateProductDto with validation decorators

--- src/products/create-product.dto.ts
+++ src/products/create-product.dto.ts
@@ -1,1 +1,18 @@
+import { IsString, IsNumber, IsOptional } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  name: string;
+
+  @IsNumber()
+  price: number;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsNumber()
+  stock?: number;
+}
 
