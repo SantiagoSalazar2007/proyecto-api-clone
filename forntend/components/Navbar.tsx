04c43bf3216ed077e00f0654d88ebd142e35c7e2
Donpichalinda <onlygameswhitme@gmail.com>
2025-10-22 16:39:04 -0500

    Add Navbar component with navigation links

--- forntend/components/Navbar.tsx
+++ forntend/components/Navbar.tsx
@@ -1,1 +1,14 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Link href="/">Inicio</Link> |{" "}
+      <Link href="/products">Productos</Link> |{" "}
+      <Link href="/cart">Carrito</Link> |{" "}
+      <Link href="/orders">Órdenes</Link> |{" "}
+      <Link href="/auth/login">Login</Link>
+    </nav>
+  );
+}
 
