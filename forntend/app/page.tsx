a9e3bd57c3ef2c65dc46d6964e9767ce498afa19
Donpichalinda <onlygameswhitme@gmail.com>
2025-10-22 17:37:56 -0500

    Delete forntend directory

--- forntend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Bienvenido al Proyecto API Clone</h1>
-      <p>Explora productos, agrega al carrito y realiza tus compras.</p>
-    </div>
-  );
-}
-
