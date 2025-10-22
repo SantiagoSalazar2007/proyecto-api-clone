f3920b94776c29afbcc16e2aff1eb4477a330eb2
Donpichalinda <onlygameswhitme@gmail.com>
2025-10-22 16:38:25 -0500

    Add HomePage component with welcome message

--- forntend/app/page.tsx
+++ forntend/app/page.tsx
@@ -1,1 +1,9 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Bienvenido al Proyecto API Clone</h1>
+      <p>Explora productos, agrega al carrito y realiza tus compras.</p>
+    </div>
+  );
+}
 
